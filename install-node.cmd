5e636ee9855ea913e668299fce75d669521bbae0
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-06-20 07:10:06 +0700

    Update install-node.cmd

--- install-node.cmd
+++ install-node.cmd
@@ -1,6 +1,10 @@
 @echo off
-sudo apt update -y
-sudo apt install nodejs npm -y
-sudo apt install gcc -y
-sudo apt install g++ -y
-sudo apt autoremove -y
+sudo su
+apt update -y
+apt upgrade -y
+apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install nodejs npm -y
+apt install gcc -y
+apt install g++ -y
+apt autoremove -y
