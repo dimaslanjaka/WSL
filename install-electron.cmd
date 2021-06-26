48a5fc9b2316d1b161e827a8ae1f35c5d54280ae
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-06-26 10:25:34 +0700

    Update install-electron.cmd

--- install-electron.cmd
+++ install-electron.cmd
@@ -1,5 +1,5 @@
 @echo off
 sudo apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev -y
-sudo npm install electron --unsafe-perm=true
 rm -r node_modules
-npm install --platform=win32
+sudo npm install electron --unsafe-perm=true --platform=win32
+rem npm install --platform=win32
