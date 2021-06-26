6022860828471402d76b9ca60f3e3e7cc1e55f53
dimaslanjaka <dimaslanjaka@gmail.com>
2021-06-26 11:09:07 +0700

    fix

--- install-electron.cmd
+++ install-electron.cmd
@@ -1,5 +1,8 @@
 @echo off
+
 sudo apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev -y
 rm -r node_modules
 sudo npm install electron --unsafe-perm=true --platform=win32
 rem npm install --platform=win32
+
+
