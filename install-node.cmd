e0e0caf6f98afaa5ce7ac76fb76f9f5282885fc7
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-06-26 08:43:21 +0700

    Update install-node.cmd

--- install-node.cmd
+++ install-node.cmd
@@ -5,12 +5,10 @@ apt upgrade -y
 echo "Installing C++ and additional requirements for nodejs"
 apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-apt install nodejs npm -y
-apt install gcc -y
-apt install g++ -y
-apt install build-essential -y 
-echo "Updating npm"
-npm install -g npm
+aptitude -f install nodejs npm -y
+aptitude -f install gcc -y
+aptitude -f install g++ -y
+aptitude -f install build-essential -y  
 echo "Installing yarn"
 npm install -g yarn
 echo "Installing gyp"
