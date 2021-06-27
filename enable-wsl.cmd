dfd46d0db31693f0a1e1df95a1e0afc313c0e1c4
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-06-27 22:48:27 +0700

    Create enable-wsl.cmd

---
+++ enable-wsl.cmd
@@ -0,0 +1,4 @@
+rem Enable the Windows Subsystem for Linux
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+rem Enable Virtual Machine feature
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
