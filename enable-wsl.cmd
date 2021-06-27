d74fae6149978e18cb30ec10e388c55a808e6ba3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-06-27 22:50:29 +0700

    Update enable-wsl.cmd

--- enable-wsl.cmd
+++ enable-wsl.cmd
@@ -2,3 +2,5 @@ rem Enable the Windows Subsystem for Linux
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 rem Enable Virtual Machine feature
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+rem set default 
+rem wsl --set-default-version 2
